51e1b937f2a0fb5f53d6959dea11a4d344bc7069
christian <chris.geymo@gmail.com>
2014-03-18 13:59:30 +0100

    layouts

---
+++ speechreminder/src/main/java/core/september/speechreminder/config/QBQueries.java
@@ -0,0 +1,13 @@
+package core.september.speechreminder.config;
+
+/**
+ * Created by christian on 18/03/14.
+ */
+public enum QBQueries {
+    SIGNIN,
+    SIGNUP,
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE
+}
---
+++ speechreminder/src/main/java/core/september/speechreminder/helper/AuthHelper.java
@@ -0,0 +1,29 @@
+package core.september.speechreminder.helper;
+
+import com.quickblox.core.QBCallback;
+import com.quickblox.module.users.QBUsers;
+import com.quickblox.module.users.model.QBUser;
+
+import core.september.speechreminder.config.QBQueries;
+
+public class AuthHelper {
+	
+	public static void singIn(String login, String password, QBCallback callback, QBQueries context) {
+        QBUser qbUser = new QBUser(login, password);
+        QBUsers.signIn(qbUser, callback, context);
+    }
+    
+    public static void signOut(QBCallback callback, QBQueries context) {
+        QBUsers.signOut(callback, context);
+    }
+
+    public static void signUp(String login, String password, QBCallback callback, QBQueries context) {
+        QBUser qbUser = new QBUser();
+        qbUser.setLogin(login);
+        qbUser.setPassword(password);
+        QBUsers.signUp(qbUser, callback, context);
+    }
+	
+	
+	}
+
