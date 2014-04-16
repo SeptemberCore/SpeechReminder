53e5ee09cf0926f9317d0a907c7d1f5b0039b0fe
theswolf <chris.geymo@gmail.com>
2014-04-16 14:17:17 +0200

    Branches

--- speechreminder/src/main/java/core/september/speechreminder/activities/SettingsActivity.java
+++ speechreminder/src/main/java/core/september/speechreminder/activities/SettingsActivity.java
@@ -166,6 +166,7 @@ public class SettingsActivity extends PreferenceActivity {
         getPreferenceScreen().addPreference(fakeHeader);
         addPreferencesFromResource(R.xml.pref_notification);
 
+
         // Add 'data and sync' preferences, and a corresponding header.
         fakeHeader = new PreferenceCategory(this);
         fakeHeader.setTitle(R.string.pref_header_data_sync);
